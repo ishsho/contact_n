8172ae7d8699165ace0d6001499b84f95f84627e
ishsho <ishsho1203@gmail.com>
2023-02-06 19:06:53 +0900

    投稿機能の実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,13 +6,18 @@ class TopicsController < ApplicationController
   end
 
   def create
-    Topic.create(topic_params)
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def topic_params
-    params.require(:topic).permit(:topic_title)
+    params.require(:topic).permit(:title_name).merge(user_id: current_user.id)
   end
 
 end
