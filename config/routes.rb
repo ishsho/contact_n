c075da4543f252a41b8ae3d7ebd6c32012dbdb50
ishsho <ishsho1203@gmail.com>
2023-02-07 22:48:25 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'topics#index'
-  resources :topics, only: [:index, :create]
+  resources :topics, only: [:index, :create, :edit ,:update]
 end
