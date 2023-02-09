177c1fa9ecbf6f0059b38a40c5965e42d82bd75c
ishsho <ishsho1203@gmail.com>
2023-02-09 17:16:14 +0900

    new contentへのリンク作成(プルダウン内)

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,10 +1,22 @@
 class ContentsController < ApplicationController
 
-  def index
+  def new
     @contents = Content.all
   end
 
   def create
+    @content = Content.new(topic_params)
+    if @content.save
+      redirect_to root_path(anchor: 'lists')
+    else
+      render :index
+    end
+  end
+  
+  private
+
+  def topic_params
+    params.require(:content).permit(:title, :text).merge(user_id: current_user.id, topic_id: params[:topic_id])
   end
 
 end
