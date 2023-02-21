132a9b3e45eb5b26c69efb922140ec1ce81fed4a
ishsho <ishsho1203@gmail.com>
2023-02-21 21:52:25 +0900

    Update topic_spec.rb

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -28,6 +28,12 @@ RSpec.describe Topic, type: :model do
         expect(@topic.errors.full_messages).to include("Title nameは25文字以内で入力してください")
       end
 
+      it 'ユーザーが紐づいていなければ投稿できない' do
+        @topic.user = nil
+        @topic.valid?
+        expect(@topic.errors.full_messages).to include("Userを入力してください")
+      end
+
     end
   end
 
