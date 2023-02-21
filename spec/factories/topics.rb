d639c1dd574bbd63d29ed7ebaf14129f4ed7eac9
ishsho <ishsho1203@gmail.com>
2023-02-21 18:52:11 +0900

    topic_spec.rbの作成

---
+++ spec/factories/topics.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :topic do
+    title_name {'aaa'}
+    
+    association :user 
+
+  end
+end
---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+RSpec.describe Topic, type: :model do
+  before do
+    @topic = FactoryBot.build(:topic)
+  end
+
+  describe 'HEADINGの保存' do
+
+    context 'HEADINGが投稿できる場合' do
+
+      it 'HEADINGタイトルを投稿できる' do
+        expect(@topic).to be_valid
+      end
+      
+    end
+
+    context 'HEADINGが投稿できない場合' do
+
+      it 'HEADINGタイトルが空では投稿できない' do
+        @topic.title_name = ''
+        @topic.valid?
+        expect(@topic.errors.full_messages).to include("Title nameを入力してください")
+      end
+
+      it 'HEADINGタイトルが26文字以上だと投稿できない' do
+        @topic.title_name = 'a' * 26
+        @topic.valid?
+        expect(@topic.errors.full_messages).to include("Title nameは25文字以内で入力してください")
+      end
+
+    end
+  end
+
+end
