5ce975ff82bafb1cf6331c8ff932b48876c31400
ishsho <ishsho1203@gmail.com>
2023-02-24 19:14:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :contents, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 
-  resources :rooms, only: [:new, :create, :destroy] do
+  resources :rooms, only: [:index, :new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 
